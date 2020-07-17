f04081cd0784e2535f6fdc359adcc22a6985e561
Claude Roy <claude.roy790@gmail.com>
2020-07-16 21:38:42 -0500

    Add release 1.0.1 to test auto update.

--- Organic-Wizard/Organic-Wizard/Forms/MainForm.Designer.cs
+++ Organic-Wizard/Organic-Wizard/Forms/MainForm.Designer.cs
@@ -85,6 +85,8 @@
             this.chkUseSupportSkills = new System.Windows.Forms.CheckBox();
             this.btnStartStop = new System.Windows.Forms.Button();
             this.btnDebug = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tpAttack.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trkRecoverySkillPercent)).BeginInit();
@@ -764,11 +766,31 @@
             this.btnDebug.Visible = false;
             this.btnDebug.Click += new System.EventHandler(this.btnDebug_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(376, 600);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 50);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(493, 602);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 57);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(626, 662);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnDebug);
             this.Controls.Add(this.btnStartStop);
             this.Controls.Add(this.tabControl1);
@@ -849,5 +871,7 @@
         private System.Windows.Forms.CheckBox chkParty1;
         private System.Windows.Forms.TextBox txtSuppSkill1;
         private System.Windows.Forms.Button btnDebug;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
